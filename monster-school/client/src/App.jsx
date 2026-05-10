58324ff6bd1056af0ba69c42c5628697d3b57b1c
omicus327-creator <omicus327@gmail.com>
2026-05-09 19:26:31 -0600

    commit

--- monster-school/client/src/App.jsx
+++ monster-school/client/src/App.jsx
@@ -43,6 +43,8 @@ export default function App() {
   const [lobbyHostName, setLobbyHostName] = useState('');
   const [chatMessages, setChatMessages] = useState([]);
   const [disconnectMsg, setDisconnectMsg] = useState('');
+  const [nightEliminated, setNightEliminated] = useState(null);
+  const [voteResult, setVoteResult] = useState(null);
 
   useEffect(() => {
     if (fading) {
